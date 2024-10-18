c6cf14750f58185162411e2f51a0c1fd925ce238
DannnyLe <danny.le@playstudios.asia>
2024-10-19 00:23:15 +0700

    fix stuff + add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 
-import Welcome from "./components/WelcomeScreen/Welcome.vue"
+// import Welcome from "./components/WelcomeScreen/Welcome.vue"
 </script>
 
 <template>
     <!-- <h1 class="text-2xl">hello</h1> -->
     <main>
-        <Welcome/>
+        <!-- <Welcome/> -->
+        <RouterView />
     </main>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import Login from './components/Authenticate/Login.vue'
 
-createApp(App).mount('#app')
+import { createMemoryHistory, createWebHistory, createRouter } from 'vue-router'
+const routes = [
+  { path: '/login', name: "login",  component: Login },
+  { path: '/', redirect: {name: "login"}},
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes,
+})
+
+
+createApp(App).use(router).mount('#app')
